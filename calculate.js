d41cc91e26e6de55acfb907982d593eeffe8defb
Omar Valikhanov <omar.velikhanov@gmail.com>
2020-03-26 21:13:11 +0400

    submit

---
+++ calculate.js
@@ -0,0 +1,209 @@
+let jsonData = '[{"bankName":"Газпромбанк","investName":"Ваш успех","currency":"RUB","incomeType":6.22,"sumMin":50000,"sumMax":null,"termMin":12,"termMax":12,"canDeposit":false},{"bankName":"Кредит Европа Банк","investName":"Оптимальный на 2 года","currency":"RUB","incomeType":6.45,"sumMin":100000,"sumMax":null,"termMin":24,"termMax":24,"canDeposit":false},{"bankName":"Банк Зенит","investName":"Праздничный 500+","currency":"RUB","incomeType":6,"sumMin":30000,"sumMax":null,"termMin":17,"termMax":17,"canDeposit":false},{"bankName":"Еврофинанс Моснарбанк","investName":"Классический","currency":"RUB","incomeType":6.95,"sumMin":30000,"sumMax":null,"termMin":12,"termMax":24,"canDeposit":false},{"bankName":"Джей энд Ти Банк","investName":"Магнус-Онлайн","currency":"RUB","incomeType":6.8,"sumMin":100000,"sumMax":null,"termMin":6,"termMax":6,"canDeposit":false},{"bankName":"МТС Банк","investName":"В вашу пользу","currency":"RUB","incomeType":6.75,"sumMin":50000,"sumMax":null,"termMin":12,"termMax":12,"canDeposit":true},{"bankName":"Эс-Би-Ай Банк","investName":"Свои правила Онлайн","currency":"RUB","incomeType":6.7,"sumMin":30000,"sumMax":30000000,"termMin":24,"termMax":24,"canDeposit":false},{"bankName":"Банк Уралсиб","investName":"Прогноз отличный","currency":"RUB","incomeType":6.7,"sumMin":100000,"sumMax":null,"termMin":37,"termMax":37,"canDeposit":false},{"bankName":"Инвестторгбанк","investName":"ИТБ-Постоянный доход","currency":"RUB","incomeType":6.6,"sumMin":50000,"sumMax":null,"termMin":37,"termMax":37,"canDeposit":false},{"bankName":"Транскапиталбанк","investName":"ТКБ.Постоянный доход","currency":"RUB","incomeType":6.6,"sumMin":50000,"sumMax":null,"termMin":37,"termMax":37,"canDeposit":false},{"bankName":"Совкомбанк","investName":"Зимний праздник с Халвой","currency":"RUB","incomeType":5.6,"sumMin":50000,"sumMax":null,"termMin":2,"termMax":2,"canDeposit":true},{"bankName":"Агророс","investName":"Медовый месяц","currency":"RUB","incomeType":5.51,"sumMin":20000,"sumMax":null,"termMin":1,"termMax":1,"canDeposit":true},{"bankName":"Росдорбанк","investName":"Онлайн-1","currency":"RUB","incomeType":5.1,"sumMin":100000,"sumMax":150000000,"termMin":1,"termMax":1,"canDeposit":true},{"bankName":"Национальный Стандарт","investName":"Сберегательный стандарт","currency":"RUB","incomeType":5.1,"sumMin":100000,"sumMax":null,"termMin":1,"termMax":3,"canDeposit":true},{"bankName":"Россия","investName":"Морозные узоры","currency":"RUB","incomeType":5,"sumMin":100000,"sumMax":null,"termMin":1,"termMax":1,"canDeposit":true},{"bankName":"Кузнецкий Мост","investName":"Накопительный","currency":"RUB","incomeType":4.85,"sumMin":50000,"sumMax":null,"termMin":1,"termMax":3,"canDeposit":true},{"bankName":"Тексбанк","investName":"Универсальный","currency":"RUB","incomeType":4.6,"sumMin":10000,"sumMax":null,"termMin":1,"termMax":1,"canDeposit":true},{"bankName":"Морской Банк","investName":"Правильным курсом +","currency":"RUB","incomeType":4.55,"sumMin":100000,"sumMax":null,"termMin":1,"termMax":3,"canDeposit":true},{"bankName":"Норвик Банк","investName":"Лаконичный","currency":"RUB","incomeType":4.5,"sumMin":500,"sumMax":50000000,"termMin":1,"termMax":1,"canDeposit":true},{"bankName":"Промсельхозбанк","investName":"Конструктор","currency":"RUB","incomeType":4.5,"sumMin":10000,"sumMax":null,"termMin":1,"termMax":3,"canDeposit":true},{"bankName":"Акибанк","investName":"Онлайн","currency":"RUB","incomeType":6.5,"sumMin":1000,"sumMax":null,"termMin":6,"termMax":6,"canDeposit":true},{"bankName":"Банк БКФ","investName":"Ключевой стандарт","currency":"RUB","incomeType":6.5,"sumMin":100000,"sumMax":null,"termMin":6,"termMax":13,"canDeposit":true},{"bankName":"Экспобанк","investName":"Специальный (в конце срока)","currency":"RUB","incomeType":6.35,"sumMin":50000,"sumMax":10000000,"termMin":6,"termMax":6,"canDeposit":true},{"bankName":"Инвестторгбанк","investName":"ИТБ-Пополняемый","currency":"RUB","incomeType":6.15,"sumMin":50000,"sumMax":30000000,"termMin":6,"termMax":6,"canDeposit":true},{"bankName":"Транскапиталбанк","investName":"ТКБ.Пополняемый","currency":"RUB","incomeType":6.15,"sumMin":50000,"sumMax":30000000,"termMin":6,"termMax":6,"canDeposit":true},{"bankName":"Евроазиатский Инвестиционный Банк","investName":"Классика","currency":"RUB","incomeType":6.1,"sumMin":100000,"sumMax":null,"termMin":6,"termMax":12,"canDeposit":true},{"bankName":"Тимер Банк","investName":"Надежный выбор","currency":"RUB","incomeType":6,"sumMin":10000,"sumMax":null,"termMin":6,"termMax":6,"canDeposit":true},{"bankName":"Евразийский Банк","investName":"TURBO MAXIMUM","currency":"RUB","incomeType":6,"sumMin":30000,"sumMax":299999,"termMin":6,"termMax":6,"canDeposit":true},{"bankName":"Таврический Банк","investName":"Достижимый (онлайн)","currency":"RUB","incomeType":6,"sumMin":50000,"sumMax":null,"termMin":6,"termMax":6,"canDeposit":true},{"bankName":"Экспобанк","investName":"Юбилейный 25 (в конце срока)","currency":"RUB","incomeType":6.5,"sumMin":100000,"sumMax":20000000,"termMin":12,"termMax":12,"canDeposit":true},{"bankName":"Крокус-Банк","investName":"Ежемесячный доход","currency":"RUB","incomeType":6.35,"sumMin":50000,"sumMax":null,"termMin":12,"termMax":12,"canDeposit":true},{"bankName":"Промсельхозбанк","investName":"Ваш выбор","currency":"RUB","incomeType":6.3,"sumMin":10000,"sumMax":null,"termMin":12,"termMax":12,"canDeposit":true},{"bankName":"Нацинвестпромбанк","investName":"Прибыльный","currency":"RUB","incomeType":6.3,"sumMin":50000,"sumMax":null,"termMin":12,"termMax":12,"canDeposit":true},{"bankName":"Ишбанк","investName":"Накопительный","currency":"RUB","incomeType":6.25,"sumMin":100000,"sumMax":null,"termMin":12,"termMax":12,"canDeposit":true},{"bankName":"Примсоцбанк","investName":"Новогодний чулок (333 дня)","currency":"RUB","incomeType":6.2,"sumMin":10000,"sumMax":null,"termMin":11,"termMax":11,"canDeposit":true},{"bankName":"Еврофинанс Моснарбанк","investName":"Пополняемый","currency":"RUB","incomeType":6.75,"sumMin":1000000,"sumMax":null,"termMin":12,"termMax":24,"canDeposit":true},{"bankName":"Евроазиатский Инвестиционный Банк","investName":"VIP","currency":"RUB","incomeType":6.35,"sumMin":1000000,"sumMax":null,"termMin":9,"termMax":12,"canDeposit":true},{"bankName":"Российская Финансовая Корпорация","investName":"Универсальный","currency":"RUB","incomeType":6,"sumMin":5000,"sumMax":null,"termMin":3,"termMax":3,"canDeposit":true},{"bankName":"Московский Кредитный Банк","investName":"МЕГА Онлайн","currency":"RUB","incomeType":5.8,"sumMin":1000,"sumMax":null,"termMin":3,"termMax":5,"canDeposit":true},{"bankName":"Александровский","investName":"Черника 19/20","currency":"RUB","incomeType":5.6,"sumMin":20000,"sumMax":null,"termMin":3,"termMax":3,"canDeposit":true},{"bankName":"Финанс Бизнес Банк","investName":"Мандариновый!","currency":"RUB","incomeType":5.6,"sumMin":50000,"sumMax":null,"termMin":3,"termMax":3,"canDeposit":true},{"bankName":"ЦентроКредит","investName":"Доход Плюс","currency":"USD","incomeType":1.15,"sumMin":5000,"sumMax":null,"termMin":3,"termMax":3,"canDeposit":true},{"bankName":"Совкомбанк","investName":"Удобный (в долларах)","currency":"USD","incomeType":1,"sumMin":500,"sumMax":null,"termMin":3,"termMax":6,"canDeposit":true},{"bankName":"Веста","investName":"Веста - Копилка","currency":"USD","incomeType":1,"sumMin":10000,"sumMax":null,"termMin":3,"termMax":6,"canDeposit":true},{"bankName":"Славия","investName":"Славный Капитал","currency":"USD","incomeType":0.85,"sumMin":5000,"sumMax":null,"termMin":3,"termMax":4,"canDeposit":true},{"bankName":"Роскосмосбанк","investName":"Комфортный","currency":"USD","incomeType":0.8,"sumMin":500,"sumMax":null,"termMin":3,"termMax":6,"canDeposit":true},{"bankName":"ФорБанк","investName":"Срочный накопительный","currency":"USD","incomeType":0.8,"sumMin":10000,"sumMax":500000,"termMin":3,"termMax":3,"canDeposit":true},{"bankName":"Московский Областной Банк","investName":"Гарантированный доллар","currency":"USD","incomeType":0.75,"sumMin":50,"sumMax":null,"termMin":4,"termMax":4,"canDeposit":true},{"bankName":"Объединенный Резервный Банк","investName":"ОРБ-Накопительный (в конце срока)","currency":"USD","incomeType":1.6,"sumMin":1000,"sumMax":null,"termMin":12,"termMax":12,"canDeposit":true},{"bankName":"Банк Агора","investName":"Срочный","currency":"USD","incomeType":1.5,"sumMin":1000,"sumMax":null,"termMin":12,"termMax":12,"canDeposit":true},{"bankName":"Тинькофф Банк","investName":"СмартВклад (с повышенной ставкой)","currency":"USD","incomeType":1.5,"sumMin":1000,"sumMax":null,"termMin":12,"termMax":12,"canDeposit":true},{"bankName":"Первый Инвестиционный Банк","investName":"Закон сохранения","currency":"USD","incomeType":1.5,"sumMin":1000,"sumMax":null,"termMin":12,"termMax":12,"canDeposit":true},{"bankName":"Новый Век","investName":"Сберегательный","currency":"USD","incomeType":1.5,"sumMin":5000,"sumMax":20000,"termMin":12,"termMax":12,"canDeposit":true}]';
+
+class Application {
+    /*реализующий обработку нажатия на кнопку, 
+    получение введенных данных и отображение результатов.*/
+    constructor() {
+        this.initialAmountInput = document.getElementById('initialAmount');
+        this.monthlyRefillInput = document.getElementById('monthlyRefill');
+        this.periodInput = document.getElementById('period');
+        this.currencyInput = document.getElementById('currency');
+
+        const self = this;
+        document.querySelector('[name=submit]').addEventListener('click', function () {
+            self.handleButtonClick();
+        });
+    }
+
+    handleButtonClick() {
+        const initialAmount = Number(this.initialAmountInput.value);
+        const monthlyRefill = Number(this.monthlyRefillInput.value);
+        const period = Number(this.periodInput.value);
+        const currency = this.currencyInput.value;
+        const container = document.querySelector('table');
+
+        const clientQuery = new Deposit(initialAmount, monthlyRefill, period, currency);
+        if (clientQuery.check) {
+            const bestSuggestions = new BankProduct(clientQuery);
+            const calculator = new Calculator(bestSuggestions, clientQuery);
+            const table = calculator.getTable();
+
+            if (table.length === 1) {
+                container.innerHTML = 'Нет подходящих результатов!';
+            } else {
+                container.innerHTML = table.join('');
+            }
+        }else{
+            container.innerHTML = '';
+        }
+    }
+}
+
+class Calculator {
+    /* инициализирующийся массивом продуктов 
+    BankProduct и вычисляющий наиболее выгодный вариант. */
+    constructor(bestSuggestions, { initialAmount, monthlyRefill, period, currency }) {
+        this.bestSuggestions = bestSuggestions;
+        this.initialAmount = initialAmount;
+        this.monthlyRefill = monthlyRefill;
+        this.period = period;
+        this.currency = currency;
+    }
+    getTable() {
+        const self = this;
+        const bestSuggestions = this.bestSuggestions;
+        return this.generateTable(bestSuggestions, self);
+    }
+    generateTable(bestSuggestions, clientData) {
+        const table = [];
+        table[0] = '<tr><th>Название банка</th><th>Вклад</th><th>Процент</th><th>Итоговая сумма</th></tr>';
+        for (let i = 0; i < bestSuggestions.length; i++) {
+            let bankName = bestSuggestions[i].bankName;
+            let investName = bestSuggestions[i].investName;
+            let percent = bestSuggestions[i].incomeType;
+            let finalAmount = this.calculateFinalAmount(bestSuggestions[i], clientData);
+            table.push(this.generateRow(bankName, investName, percent, finalAmount));
+        }
+        return table;
+    }
+    generateRow(bankName, investName, percent, finalAmount) {
+        const bankNamePart = '<td>' + bankName + '</td>';
+        const investNamePart = '<td>' + investName + '</td>';
+        const percentPart = '<td>' + percent + '</td>';
+        const finalAmountPart = '<td>' + finalAmount + '</td>';
+
+        let row = '<tr>' + bankNamePart + investNamePart + percentPart + finalAmountPart + '</tr>';
+        return row;
+    }
+    calculateFinalAmount(suggestion, clientData) {
+        let finalAmount = clientData.initialAmount;
+        for (let i = 0; i < clientData.period; i++) {
+            if (i > 0 && i < clientData.period) {
+                finalAmount += finalAmount * suggestion.incomeType / 100 / 12 + clientData.monthlyRefill;
+            } else {
+                finalAmount += finalAmount * suggestion.incomeType / 100 / 12;
+            }
+        }
+        return Math.round(finalAmount);
+    }
+}
+
+class BankProduct {
+    /* реализующий свойства и функциональность банковского предложения по вкладу */
+    constructor({ initialAmount, monthlyRefill, period, currency }) {
+        const data = JSON.parse(jsonData);
+        const product = { initialAmount, monthlyRefill, period, currency };
+
+        return this.filterProduct(data, product);
+
+    }
+    filterProduct(data, product) {
+        // возвращает отфильтрованное предложение по всем критериям
+        let curFilter = this.filterForCurrency(data, product);
+        let isRefillFilter = this.filterForRefill(curFilter, product);
+        let minTermFilter = this.filterForMinPeriod(isRefillFilter, product);
+        let maxTermFilter = this.filterForMaxPeriod(minTermFilter, product);
+        let minAmountFilter = this.filterForMinAmount(maxTermFilter, product);
+        let maxAmountFilter = this.filterForMaxAmount(minAmountFilter, product);
+
+        const maxPercent = this.getMaxPercent(maxAmountFilter);
+        const bestSuggestions = this.getBestSuggestions(maxAmountFilter, maxPercent);
+        return bestSuggestions;
+    }
+    filterForCurrency(data, product) {
+        // фильтр по валюте
+        return data.filter(suggestion => {
+            return suggestion.currency === product.currency;
+        });
+    }
+    filterForRefill(data, product) {
+        // фильтр по возможности пополнять депозитный счёт
+        if (product.monthlyRefill > 0) {
+            return data.filter(suggestion => {
+                return suggestion.canDeposit === true;
+            });
+        } else {
+            return data.filter(suggestion => {
+                return suggestion.canDeposit === false;
+            });
+        }
+    }
+    filterForMinAmount(data, product) {
+        // фильтр по минимальной депозитной ставке
+        return data.filter(suggestion => {
+            return product.initialAmount >= suggestion.sumMin;
+        });
+    }
+    filterForMaxAmount(data, product) {
+        // фильтр по максимальной возможности увеличения депозитного баланса
+        return data.filter(suggestion => {
+            if (suggestion.sumMax != null) {
+                let calculate = new Calculator(suggestion, product);
+                let finalAmount = calculate.calculateFinalAmount(suggestion, product);
+                return finalAmount <= suggestion.sumMax;
+            } else {
+                return true;
+            }
+        });
+    }
+    filterForMinPeriod(data, product) {
+        // фильтр по минимальному сроку
+        return data.filter(suggestion => {
+            return product.period >= suggestion.termMin;
+        });
+    }
+    filterForMaxPeriod(data, product) {
+        // фильтр по максимальному сроку
+        return data.filter(suggestion => {
+            return product.period <= suggestion.termMax;
+        });
+    }
+    getMaxPercent(suggestList) {
+        // получение максимальной процентной ставки
+        const percentList = suggestList.reduce((result, suggestion) => {
+            result.push(suggestion.incomeType);
+            return result;
+        }, []);
+        return Math.max.apply(null, percentList);
+    }
+    getBestSuggestions(suggestList, maxPercent) {
+        // получение лучших предложений
+        return suggestList.filter(suggestion => {
+            return suggestion.incomeType === maxPercent;
+        });
+    }
+}
+
+class Deposit {
+    /* реализующий свойства и функциональность
+     вклада, который хотел бы открыть клиент. */
+    constructor(initialAmount, monthlyRefill, period, currency) {
+        this.initialAmount = initialAmount;
+        this.monthlyRefill = monthlyRefill;
+        this.period = period;
+        this.currency = currency.toUpperCase();
+        this.check = true;
+
+        if (this.findError() != '') {
+            alert(this.findError());
+            this.check = false;
+        }
+    }
+    findError() {
+        let errorLog = '';
+        if (this.initialAmount < 0 || isNaN(this.initialAmount)) {
+            errorLog += 'Начальная сумма должна быть не отрицательным числом.\n';
+        }
+        if (this.monthlyRefill < 0 || isNaN(this.monthlyRefill)) {
+            errorLog += 'Сумма ежемесячного пополнения должна быть не отрицательным числом.\n';
+        }
+        if (this.period < 0 || Math.trunc(this.period) != this.period || isNaN(this.period)) {
+            errorLog += 'Срок вклада должна быть положительным целым числомю\n';
+        }
+        if (!(this.currency !== 'RUB' ^ this.currency !== 'USD')) {
+            errorLog += 'Поле "валюта вклада" содержит ошибку ввода.';
+        }
+        return errorLog;
+    }
+}
+new Application();
